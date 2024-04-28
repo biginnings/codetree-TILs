37088b967ff6fb654b37ce6ce79831e58282035a
biginnings <144890310+biginnings@users.noreply.github.com>
2024-04-28 15:02:45 +0900

    Title: 코드트리 오마카세, Time: 1526ms, Memory: 4MB, Status: Time Limit Exceed - CodeTree

--- 240428/코드트리 오마카세/codetree-omakase.cpp
+++ 240428/코드트리 오마카세/codetree-omakase.cpp
@@ -30,6 +30,9 @@ unordered_map<string, Point> sushi;
 
 int main() {
     // 여기에 코드를 작성해주세요.
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int a = 0;
 
     cin >> L >> Q;
