069a39a3845aa6d58e25b46d99e095c936b0b297
biginnings <144890310+biginnings@users.noreply.github.com>
2024-04-30 14:58:39 +0900

    Title: 코드트리 오마카세, Time: 179ms, Memory: 16MB, Status: Passed - CodeTree

--- 240430/코드트리 오마카세/codetree-omakase.cpp
+++ 240430/코드트리 오마카세/codetree-omakase.cpp
@@ -8,119 +8,107 @@
 
 using namespace std;
 
-struct t_x {
-    int t, x, next_t;
-};
-
-// 초밥 현재위치(t 를 현재시간에 맞춘뒤 next_t 를 기준으로 정렬) <-> 게스트 위치(0 으로 고정)
-struct Guest {
-    int x, n;
+struct Grid {
+    int q, time, x, sushi_cnt, guest_cnt;
 };
 
 struct Point {
-    Guest guest;
-    priority_queue<int, vector<int>, greater<int>> pq;
-    queue<t_x> q;
+    int time, x, n;
 };
 
-int L, Q;
-int timer, x, n;
-int cnt = 0, guest_cnt = 0;
+struct Sushi {
+    int time, x;
+    string name;
+};
+
+struct SSU {
+    int time;
+    string name;
+
+    bool operator<(SSU other) const {
+        return time > other.time;
+    }
+};
+
+int L, Q, t, x, n;
 string name;
-unordered_map<string, Point> sushi;
+Grid grid[100000];
+unordered_map<string, Point> guest;
+queue<Sushi> q;
+priority_queue<SSU> pq;
+int sushi_cnt, guest_cnt, sushi_m, guest_m;
 
 int main() {
     // 여기에 코드를 작성해주세요.
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int a = 0;
 
     cin >> L >> Q;
 
+    int a;
     for (int i = 0; i < Q; i++) {
         cin >> a;
 
-        // 회전
-
-        // 입력 처리
         if (a == 100) {
-            cin >> timer >> x >> name;
+            cin >> t >> x >> name;
+            sushi_cnt++;
 
-            int next_time = 0;
-            if (sushi[name].guest.n != 0) {
-                if (sushi[name].guest.x - x < 0) {
-                    next_time = timer + sushi[name].guest.x + L - x;
-                }
-                else {
-                    next_time = timer + sushi[name].guest.x - x;
-                }
-                sushi[name].pq.push(next_time);
-                cnt++;
-            }
-            else {
-                sushi[name].q.push({ timer, x, INT_MAX });
-                cnt++;
-            }
+            q.push({ t, x, name });
         }
         else if (a == 200) {
-            cin >> timer >> x >> name >> n;
+            cin >> t >> x >> name >> n;
             guest_cnt++;
 
-            while (!sushi[name].q.empty()) {
-                t_x temp = sushi[name].q.front();
-                sushi[name].q.pop();
-
-                int now_x = (timer - temp.t + temp.x) % L;
-                int next_time = 0;
-                if (x - now_x < 0) {
-                    next_time = timer + x + L - now_x;
-                }
-                else {
-                    next_time = timer + x - now_x;
-                }
-                sushi[name].pq.push(next_time);
-            }
-            sushi[name].guest = { x, n };
-        }
-        else if (a == 300) {
-            cin >> timer;
-
-            if (cnt == 0 || guest_cnt == 0) {
-                cout << guest_cnt << " " << cnt << "\n";
-                continue;
-            }
-
-            for (auto i = sushi.begin(); i != sushi.end(); i++) {
-                if (i->second.guest.n == 0) continue;
-                string now = i->first;
-
-                while (!sushi[now].pq.empty()) {
-                    int temp = sushi[now].pq.top();
-
-                    if (temp <= timer) {
-                        sushi[now].pq.pop();
-                        sushi[now].guest.n--;
-                        cnt --;
-                        if (sushi[now].guest.n == 0) {
-                            guest_cnt--;
-                            break;
-                        }
-                    }
-                    else {
-                        break;
-                    }
-                }
-            }
-
-            cout << guest_cnt << " " << cnt << "\n";
+            guest[name] = { t, x, n };
         }
         else {
-            cout << "error\n";
-            return 0;
+            cin >> t;
+            grid[i] = { a, t, 0, sushi_cnt, guest_cnt};
+        }
+    }
+
+    while (!q.empty()) {
+        Sushi now = q.front();
+        q.pop();
+
+        int t_gap = guest[now.name].time - now.time;
+
+        if (t_gap > 0) {
+            now.time += t_gap;
+            now.x = (now.x + t_gap) % L;
         }
 
+        int eated = guest[now.name].x - now.x;
+
+        if (eated < 0) {
+            eated += L;
+        }
+        
+        eated += now.time;
+        pq.push({ eated, now.name });
     }
+
+    for (int i = 0; i < Q; i++) {
+        if (grid[i].q != 300) continue;
+
+        while (!pq.empty()) {
+            SSU now = pq.top();
+
+            if (now.time <= grid[i].time) {
+                pq.pop();
+                sushi_m++;
+                guest[now.name].n--;
+                if (guest[now.name].n == 0) guest_m++;
+            }
+            else {
+                break;
+            }
+        }
+
+        cout << grid[i].guest_cnt - guest_m << " " << grid[i].sushi_cnt - sushi_m << "\n";
+    }
+
 
     return 0;
 }
