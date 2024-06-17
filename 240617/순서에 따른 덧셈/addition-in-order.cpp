1caabd63c15766c347d6f09567888ce8fb3e5b52
biginnings <144890310+biginnings@users.noreply.github.com>
2024-06-17 22:42:52 +0900

    Title: 순서에 따른 덧셈, Time: 48ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240617/순서에 따른 덧셈/addition-in-order.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+priority_queue<int> pq;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int a = 0;
+    for(int i = 0; i < n; i++) {
+        cin >> a;
+
+        pq.push(a);
+    }
+
+    int b = 0;
+    int i = 1;
+    long long result = 0;
+    while(!pq.empty()) {
+        b = pq.top();
+        pq.pop();
+
+        if(b - (i - 1) > 0) {
+            result += b - (i - 1);
+        } else {
+            break;
+        }
+        i++;
+    }
+
+    cout << result;
+
+    return 0;
+}
